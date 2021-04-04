48f7938de579184b348bec6a17aa652101ecc60a
Mark Safronov <hijarian@gmail.com>
2021-04-04 17:38:26 +0300

    Added Helmet and CORS support.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
+import helmet from 'fastify-helmet';
+
 import { ApplicationModule } from 'src/application/application.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     ApplicationModule,
-    new FastifyAdapter()
+    new FastifyAdapter({ logger: true })
   );
+  app.enableCors({ origin: true, credentials: true });
+  app.register(helmet);
   await app.listen(3000, '0.0.0.0');
 }
 
