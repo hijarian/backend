145cada91cffc9a2074b64993334a23e3fa41f0d
Mark Safronov <hijarian@gmail.com>
2021-04-06 14:01:24 +0300

    Integrated Jest to the toolchain

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
 module.exports = {
   root: true,
-  // Enable Typescript parsing
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:prettier/recommended',
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      // Enable Typescript parsing
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:prettier/recommended',
+      ]
+    },
+    {
+      files: ["**/*.json"],
+      "extends": ["plugin:json/recommended"]
+    }
   ],
   env: {
     node: true,
