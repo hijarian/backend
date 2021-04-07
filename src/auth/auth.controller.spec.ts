55525841a895792fefb789033141386c8f4f4738
Mark Safronov <hijarian@gmail.com>
2021-04-07 20:37:16 +0300

    Added a proper test scenario for auth.

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,40 @@
-describe('It', () => {
-  it('is alive, too', () => {
-    expect(2 * 3).toEqual(6);
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Test } from '@nestjs/testing';
+import { AuthModule } from './auth.module';
+
+describe('Authentication', () => {
+  let app: NestFastifyApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AuthModule],
+    }).compile();
+    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  it('when receiving the JWT provided after login, return user data', async () => {
+    const loginResponse = await app.inject({
+      method: 'POST',
+      url: '/auth/login',
+      payload: {
+        username: 'john',
+        password: 'changeme',
+      },
+    });
+
+    const jwt = JSON.parse(loginResponse.payload).accessToken;
+    const profileResponse = await app.inject({
+      method: 'GET',
+      url: '/users/profile',
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+
+    const user = JSON.parse(profileResponse.payload);
+
+    expect(user).toEqual({ userId: 1, username: 'john' });
   });
 });
